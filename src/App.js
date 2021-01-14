ab5c647ec2c60f2b2d779ab3b76d7f31d458f70c
Karim <z5209523@ad.unsw.edu.au>
2021-01-14 15:08:58 +1100

    test

--- src/App.js
+++ src/App.js
@@ -11,52 +11,30 @@ function App() {
   return (
     <div className='App'>
       <header className='App-container'>
-        <p className='Title-text'>
-          Ratings
-        </p>
+        <p className='Title-text'>Ratings</p>
         <div className='Rating-container'>
-          <p className='Category-text'>
-            Quality
-          </p>
+          <p className='Category-text'>Quality</p>
           <RatingsBar />
           <RatingSubheading />
-          <p className='Category-text'>
-            Design
-          </p>
+          <p className='Category-text'>Design</p>
           <RatingsBar />
           <RatingSubheading />
-          <p className='Category-text'>
-            Experience
-          </p>
+          <p className='Category-text'>Experience</p>
           <RatingsBar />
           <RatingSubheading />
           <p className='Category-text'>
             Product Standouts
           </p>
-          <p className='Category-subtext'>
-            Choose up to 5 that best apply (optional)
-          </p>
+          <p className='Category-subtext'>Choose up to 5 that best apply (optional)</p>
           <ButtonSelectStandouts />
-          <p className='Category-text'>
-            <b>About You</b>
-          </p>
-          <p className='Category-text'>
-            Age Range
-          </p>
-          <p className='Category-subtext'>
-            Choose <b>one</b>
-          </p>
+          <p className='Category-text'><b>About You</b></p>
+          <p className='Category-text'>Age Range</p>
+          <p className='Category-subtext'>Choose <b>one</b></p>
           <ButtonSelectAge />
-          <p className='Category-text'>
-            Bought For
-          </p>
-          <p className='Category-subtext'>
-            Choose <b>one</b>
-          </p>
+          <p className='Category-text'>Bought For</p>
+          <p className='Category-subtext'>Choose <b>one</b> </p>
           <ButtonSelectGift />
-          <p className='Category-text'>
-            Country
-          </p>
+          <p className='Category-text'>Country</p>
           <CountrySelector placeholder='Australia' />
         </div>
         <div className='Next-container'>
